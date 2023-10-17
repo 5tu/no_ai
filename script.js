5c36ac7975ee64be49231d346266f264c8d2ad54
Stu <stu@mcomie.net>
2023-10-17 15:02:52 +0100

    a

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function searchGoogleImages() {
-  const searchTerm = document.getElementById('searchTerm').value;
-  const query = searchTerm + '  -"ai" -"stable diffusion" -"midjourney" -"open art" -"prompt hunt" -"realistic vision"';
-  const googleImagesSearchURL = generateGoogleImagesSearchURL(query);
-  console.log(googleImagesSearchURL, '_blank');
-  window.open(googleImagesSearchURL, '_blank');
-}
-
-function generateGoogleImagesSearchURL(query) {
-  const baseGoogleImagesURL = "https://www.google.com/search?tbm=isch&q=";
-  const encodedQuery = encodeURIComponent(query);
-  return `${baseGoogleImagesURL}${encodedQuery}`;
-}
+    const searchTerm = document.getElementById('searchTerm').value;
+    const query = searchTerm + '  -"ai" -"stable diffusion" -"midjourney" -"open art" -"prompt hunt" -"realistic vision"';
+    const googleImagesSearchURL = generateGoogleImagesSearchURL(query);
+    console.log(googleImagesSearchURL, '_blank');
+    window.open(googleImagesSearchURL, '_blank');
+  }
+  
+  function generateGoogleImagesSearchURL(query) {
+    const baseGoogleImagesURL = "https://www.google.com/search?tbm=isch&q=";
+    const encodedQuery = encodeURIComponent(query);
+    return `${baseGoogleImagesURL}${encodedQuery}`;
+  }
