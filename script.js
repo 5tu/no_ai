182a2e11b160b37206c8a6823ab0e481deea3534
Stu <stu@mcomie.net>
2023-10-17 12:22:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function searchGoogleImages() {
     const searchTerm = document.getElementById('searchTerm').value;
-    const query = searchTerm + '  -"ai" -"stable diffusion" -"midjourney" -"open art" -"prompt hunt" -"realistic vision";
+    const query = searchTerm + '  -"ai" -"stable diffusion" -"midjourney" -"open art" -"prompt hunt" -"realistic vision"';
     const googleImagesSearchURL = generateGoogleImagesSearchURL(query);
     console.log(googleImagesSearchURL, '_blank');
     window.open(googleImagesSearchURL, '_blank');
