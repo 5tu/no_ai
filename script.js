a1d4b15533934de1df43f330f872d3e5a9945b82
Stu <stu@mcomie.net>
2023-10-17 16:39:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+
 function searchGoogleImages() {
-    const searchTerm = document.getElementById('searchTerm').value;
-    const query = searchTerm + '  -"ai" -"stable diffusion" -"midjourney" -"open art" -"prompt hunt" -"realistic vision"';
-    const googleImagesSearchURL = generateGoogleImagesSearchURL(query);
-    console.log(googleImagesSearchURL, '_blank');
-    window.open(googleImagesSearchURL, '_blank');
-  }
-  
-  function generateGoogleImagesSearchURL(query) {
-    const baseGoogleImagesURL = "https://www.google.com/search?tbm=isch&q=";
-    const encodedQuery = encodeURIComponent(query);
-    const dropdownSize = document.getElementById('imageSize');
-    const selectedImageSizeValue = dropdownSize.value;
-    const dropdownSafeSearch = document.getElementById('safeSearch');
-    const selectedSafeSearchValue = dropdownSafeSearch.value;
-    const insertSafeTerm = "&safe="
-    return `${baseGoogleImagesURL}${encodedQuery}${selectedImageSizeValue}${insertSafeTerm}${selectedSafeSearchValue}`;
-  }
+  const searchTerm = document.getElementById('searchTerm').value;
+  const query = searchTerm + '  -"ai" -"stable diffusion" -"midjourney" -"open art" -"prompt hunt" -"realistic vision"';
+  const googleImagesSearchURL = generateGoogleImagesSearchURL(query);
+  console.log(googleImagesSearchURL, '_blank');
+  window.open(googleImagesSearchURL, '_blank');
+}
+
+function generateGoogleImagesSearchURL(query) {
+  const baseGoogleImagesURL = "https://www.google.com/search?tbm=isch&q=";
+  const encodedQuery = encodeURIComponent(query);
+  const dropdownSize = document.getElementById('imageSize');
+  const selectedImageSizeValue = dropdownSize.value;
+  const dropdownSafeSearch = document.getElementById('safeSearch');
+  const selectedSafeSearchValue = dropdownSafeSearch.value;
+  return `${baseGoogleImagesURL}${encodedQuery}${selectedImageSizeValue}${selectedSafeSearchValue}`;
+}
